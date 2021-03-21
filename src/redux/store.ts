ab188761fa2aeceae34eced219234e9cc3ef7eab
Alex Kokhan <kokhan.a.a@yandex.by>
2021-03-21 19:32:57 +0300

    Counter 3.1

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,18 @@
 import { combineReducers, createStore } from "redux"
 import {counterReducer} from "./counter-reducer";
+import {loadState, saveState} from "./localStirage";
 
 const rootReducer = combineReducers({
     counter: counterReducer })
 
 
-
 export type AppRootState = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, loadState())
+
+store.subscribe(() => {
+    saveState({
+        counter: store.getState().counter
+    })
+  localStorage.setItem('appState', JSON.stringify(store.getState()))
+})
